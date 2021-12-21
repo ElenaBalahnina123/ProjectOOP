72b6b70df01d210a803479f8bce54c3c531ec922
Elena Balakhnina <eb6640466@gmail.com>
2021-12-21 19:53:15 +0300

    формы

--- Project/SupplierEditorForm.Designer.cs
+++ Project/SupplierEditorForm.Designer.cs
@@ -35,10 +35,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 93);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(12, 70);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Сохранить";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,32 +45,30 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(14, 55);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(229, 27);
+            this.textBox1.Size = new System.Drawing.Size(201, 23);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 13);
+            this.label1.Location = new System.Drawing.Point(15, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 20);
+            this.label1.Size = new System.Drawing.Size(131, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "Добавьте поставщика:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // SupplierEditorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(329, 140);
+            this.ClientSize = new System.Drawing.Size(288, 105);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "SupplierEditorForm";
             this.Text = "SupplierEditorForm";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SupplierEditorForm_FormClosed_1);
