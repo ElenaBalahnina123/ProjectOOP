2a62d51bcf43a7557bda115a8045aebeaaf90302
Антон Слобожанинов <slobozhaninov_aa@open.ru>
2022-01-16 16:11:17 +0300

    removed unused code created new entities

--- Project/AppDbContext.cs
+++ Project/AppDbContext.cs
@@ -3,31 +3,7 @@ using ProjectOop.Entities;
 
 public class AppDbContext : DbContext
 {
-
-    // public DbSet<Article> Articles { get; set; } // артикул
-    // public DbSet<Subdivision> Divisions { get; set; } // подразделения
-
-    //public DbSet<Role> Roles { get; set; }
-    public DbSet<Employee> Employees { get; set; } // сотрудники
-                                                   // public DbSet<FinishesProductWarehouse> Finishes_product_warehouses { get; set; } // склад готовой продукции
-                                                   // public DbSet<MaterialCutting> Material_cuttings { get; set; } // материал для раскроя
-                                                   // public DbSet<Material> Material_for_sketchs { get; set; } // материал для эскиза
-                                                   //public DbSet<Position> Posts { get; set; } // должность
-                                                   // public DbSet<Product> Products { get; set; } // товар
-                                                   // public DbSet<ProductDelivery> Product_Deliverys { get; set; } //поставка товара
-                                                   // public DbSet<ProductOnWarehouse> Product_in_warehouses { get; set; } // товар на складе
-                                                   // public DbSet<RawMaterialPuchaseTransaction> Raw_material_puchase_transactions { get; set; } // транзакция закупки сырья
-                                                   // public DbSet<RawMaterialWarehouse> Raw_material_warehouses { get; set; } // cклад сырья и материалов
-                                                   // public DbSet<RawMaterialItem> Row_stocks { get; set; }
-                                                   // public DbSet<Shop> Shops { get; set; } // магазин
-                                                   // public DbSet<ShopEmployeer> Shop_Emloyeers { get; set; } // сотрудник-магазин
-    public DbSet<Sketch> Sketchs { get; set; } //Эскиз
-                                               // public DbSet<SketchMaterialization> Sketch_Materializations { get; set; } // Материализация эскиза
-                                               // public DbSet<Supplier> Suppliers { get; set; } // Поставщик
-                                               // public DbSet<GoodsDelivery> The_goods_deliverys { get; set; } //товар в поставке
-                                               // public DbSet<TransactionContents> Transaction_Contentss { get; set; } // Содержимое транзакции
-                                               // public DbSet<ResourceRequestHistoryItem> ResourceRequestHistoryItems { get; set; } // История запросов сырья
-                                               // public DbSet<ResourceRequestItem> ResourceRequestItems { get; set; } //Содержимое запроса сырья
+    public DbSet<Employee> Employees { get; set; } 
     public DbSet<ModelColor> Colors { get; set; }
 
     public AppDbContext()
@@ -47,31 +23,7 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // modelBuilder.Entity<Article>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<Subdivision>().HasIndex(e => e.ID).IsUnique();
         modelBuilder.Entity<Employee>().HasIndex(e => e.ID).IsUnique();
-
-        //modelBuilder.Entity<Role>().HasIndex(e => e.ID).IsUnique();
-
-        // modelBuilder.Entity<FinishesProductWarehouse>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<MaterialCutting>().HasIndex(e => e.ID).IsUnique();
-        //  modelBuilder.Entity<Material>().HasIndex(e => e.ID).IsUnique();
-        //modelBuilder.Entity<Position>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<Product>().HasIndex(e => e.ID).IsUnique();
-        //  modelBuilder.Entity<ProductDelivery>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<ProductOnWarehouse>().HasIndex(e => e.ID).IsUnique();
-        //  modelBuilder.Entity<RawMaterialPuchaseTransaction>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<RawMaterialWarehouse>().HasIndex(e => e.ID).IsUnique();
-        //  modelBuilder.Entity<RawMaterialItem>().HasIndex(e => e.ID).IsUnique();
-        //  modelBuilder.Entity<Shop>().HasIndex(e => e.ID).IsUnique();
-        //  modelBuilder.Entity<ShopEmployeer>().HasIndex(e => e.ID).IsUnique();
-        modelBuilder.Entity<Sketch>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<SketchMaterialization>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<Supplier>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<GoodsDelivery>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<TransactionContents>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<ResourceRequestHistoryItem>().HasIndex(e => e.ID).IsUnique();
-        // modelBuilder.Entity<ResourceRequestItem>().HasIndex(e => e.ID).IsUnique();
         modelBuilder.Entity<ModelColor>().HasIndex(e => e.ID).IsUnique();
     }
 }
--- Project/Entities.cs
+++ Project/Entities.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace ProjectOop
@@ -14,80 +15,6 @@ namespace ProjectOop
             TECHNOLOGIST // технолог, контроль качества
         }
 
-        public class Article // артикул
-        {
-            public int ID { get; set; }
-        }
-
-        public class ResourceRequestHistoryItem // История запросов сырья
-        {
-            public int ID { get; set; }
-            [Required] public DateTime Date { get; set; } // дата
-            public RawMaterialWarehouse Warehouse { get; set; } // ID склада
-        }
-
-        public class ResourceRequestItem // содержимое запроса сырья
-        {
-            public int ID { get; set; }
-
-            public int Quantity { get; set; } // количество
-
-            public Article Article { get; set; } //ID артикула
-
-            public ResourceRequestHistoryItem Request { get; set; } //ID запроса
-
-        }
-
-        public class GoodsDelivery // Товар в поставке
-        {
-            public int ID { get; set; }
-            public Product Product { get; set; } //ID товара
-            public ProductDelivery ProductDelivery { get; set; } // ID поставки товара
-            public FinishesProductWarehouse FinishesProductWarehouse { get; set; } // ID склада готовой продукции
-            public int Quantity { get; set; } // количество
-            [Required] [MaxLength(15)] public string Size { get; set; } // размер
-            public ModelColor Color { get; set; } // цвет
-        }
-
-        public class ShopEmployeer // сотрудник-магазин
-        {
-            public int ID { get; set; }
-            public Shop Shop { get; set; } //ID магазина
-        }
-
-        /*public class Position //должность
-        {
-            public int ID { get; set; }
-            [Required] public string Name { get; set; } // название
-
-            public List<Project.Forms.Role> Roles { get; set; }
-        }*/
-
-        /*public class Role
-        {
-            public int ID { get; set; }
-            [Required] public string Name { get; set; } // название
-        }*/
-
-        public class RawMaterialWarehouse // cклад сырья и материалов
-        {
-            public int ID { get; set; }
-        }
-
-        public class RawMaterialItem // Сырье на складе
-        {
-            public int ID { get; set; }
-            public RawMaterialWarehouse Warehouse { get; set; }
-            public Article Article { get; set; }
-            public int Quantity { get; set; }
-        }
-
-        public class Subdivision // подразделения
-        {
-            public int ID { get; set; }
-            [Required] public string Name { get; set; }
-        }
-
         public class Employee // сотрудник
         {
             public int ID { get; set; }
@@ -100,104 +27,13 @@ namespace ProjectOop
             [Required] public string Salary { get; set; } //оклад
 
             public Role Role { get; set; }
-
-            //            public Position post { get; set; } // должность
-        }
-
-        public class FinishesProductWarehouse // склад готовой продукции 
-        {
-            public int ID { get; set; }
-        }
-
-        public class MaterialCutting // материал для райскроя
-        {
-            public int ID { get; set; }
-            public Material MaterialForSketch { get; set; } // материал для эскиза
-            public SketchMaterialization SketchMaterialization { get; set; } // мтериализация эскиза
-            public int Quantity { get; set; } // количество
         }
 
         public class Material // материал
         {
             public int ID { get; set; }
-            public Article Article { get; set; }
             public string Name { get; set; }
 
-        }
-
-        public class Product // товар
-        {
-            public int ID { get; set; }
-            public SketchMaterialization SketchMaterialization { get; set; }
-        }
-
-        public class ProductDelivery // поставка товара
-        {
-            public int ID { get; set; }
-            public Shop Shop { get; set; }
-            [Required] public DateTime DeliveryDate { get; set; }
-        }
-
-        public class ProductOnWarehouse // товар на складе
-        {
-            public int ID { get; set; }
-            public FinishesProductWarehouse FinishesProductWarehouse { get; set; } // ID склада
-            public Product Product { get; set; }
-            public int Quantity { get; set; }
-            [Required] public int Size { get; set; }
-            public ModelColor Color { get; set; }
-        }
-
-        public class RawMaterialPuchaseTransaction // Транзакция закупки сырья
-        {
-            public int ID { get; set; }
-            [Required] public DateTime PurchaseDate { get; set; } //дата покупки
-            public Employee Employee { get; set; } // ID сотрудника
-            public Supplier Supplier { get; set; } // ID поставщика
-            public RawMaterialWarehouse RawMaterialWarehouse { get; set; } //ID склада
-        }
-
-        public class Shop // магазин
-        {
-            public int ID { get; set; }
-            [Required] public string ShopName { get; set; }
-            [Required] public string Address { get; set; }
-        }
-
-        public class Sketch //эскиз
-        {
-            public int ID { get; set; }
-            [Required] [MaxLength(50)] public string Name { get; set; }
-            public Employee Employee { get; set; } // сотрудник
-            [Required] public DateTime CreationDate { get; set; } // дата создания эскиза
-            [Required] [MaxLength(70)] public string DegreeDevelopment { get; set; } //степень разработки
-        }
-
-        public class SketchMaterialization //Материализация эскиза
-        {
-            public int ID { get; set; }
-            [Required] [MaxLength(15)] public string Size { get; set; }
-            public Article Article { get; set; }
-            public Employee Employee { get; set; }
-            [Required] public DateTime CreationDate { get; set; } // дата создания
-            [Required] public string DegreeDevelopment { get; set; } // степень разработки
-        }
-
-        public class Supplier// поставщик
-        {
-            public int ID { get; set; }
-            [Required] public string NameOrganization { get; set; }
-            [Required] public int INN { get; set; }
-
-        }
-
-        public class TransactionContents //Содержимое транзакции 
-        {
-            public int ID { get; set; }
-            public RawMaterialPuchaseTransaction RawMaterialPuchaseTransaction { get; set; }
-            public Article Article { get; set; }
-            [Required] public int Quantity { get; set; }
-            [Required] public double Price { get; set; }
         }
 
         public class ModelColor
@@ -205,6 +41,65 @@ namespace ProjectOop
             public int ID { get; set; }
             public string RgbValue { get; set; }
             public string TextName { get; set; }
+        }
+
+        public class Sketch
+        {
+            public int ID { get; set; }
+
+            public string Name { get; set; }
+
+            public string FileLocation { get; set; }
+
+            public Employee Author { get; set; }
+
+            public DateTime CreationDate { get; set; }
+        }
+
+        public class Blueprint
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+
+            public Employee Author { get; set; }
+
+            public List<Material> Materials { get; set; }
+
+            public DateTime CreationDate { get; set; }
+
+        }
+
+        public class Cut
+        {
+            public int ID { get; set; }
+
+            public Employee Author { get; set; }
+
+            public DateTime CreationDate { get; set; }
+        }
+
+        public class Sewing
+        {
+            public int ID { get; set; }
+
+            public Employee Author { get; set; }
+
+            public DateTime CreationDate { get; set; }
+        }
+
+        public class Product
+        {
+            public int ID { get; set; }
+
+            public Sketch Sketch { get; set; }
+
+            public Blueprint Blueprint { get; set; }
+
+            public Cut Cut { get; set; }
+
+            public Sewing Sewing { get; set; }
+
+            public bool QaPassed { get; set; }
         }
     }
 }
